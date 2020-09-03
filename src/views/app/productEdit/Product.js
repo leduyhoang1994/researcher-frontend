64d2c38dd44fb437d637985bf720e8b62ecd9c48
tiemhv <tiemhv@topica.edu.vn>
2020-09-03 11:34:01 +0700

    update library ant-design and antd

--- src/views/app/productEdit/Product.js
+++ src/views/app/productEdit/Product.js
@@ -9,28 +9,95 @@ import ApiController from '../../../helpers/Api';
 import { Link } from 'react-router-dom';
 
 class Product extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: []
-    };
-    this.messages = this.props.intl.messages;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: []
+        };
+        this.messages = this.props.intl.messages;
+    }
 
-  render() {
-    return (
-      <div>
-        <Fragment>
-          <Row>
-            <Colxx xxs="12">
+    render() {
+        return (
+            <div>
+                <Fragment>
+                    <Row>
+                        <Colxx xxs="12">
+                            <Breadcrumb heading="Tìm kiếm sản phẩm" match={this.props.match} />
+                            <Separator className="mb-5" />
+                        </Colxx>
+                    </Row>
+                    <Row className="mb-4">
+                        <Colxx xxs="12">
+                            <Card>
+                                <CardBody>
+                                    <CardTitle>
+                                        {__(this.messages, 'Bộ lọc')}
+                                    </CardTitle>
+
+                                    <Row>
+                                        <Colxx xxs="12">
+                                            <Label className="form-group has-float-label">
+                                                <Input
+                                                    type="text"
+                                                    onChange={e => {
+                                                        this.setState({
+                                                            search: e.target.value
+                                                        });
+                                                    }}
+                                                />
+                                                <span>
+                                                    {__(this.messages, "Tên sản phẩm")}
+                                                </span>
+                                            </Label>
+                                        </Colxx>
+                                    </Row>
+
+                                    <Row>
+                                        <Colxx xxs="12">
+                                            <Label className="form-group has-float-label">
+                                                <Select
+                                                    filterOption={createFilter({ ignoreAccents: false })}
+                                                    isMulti
+                                                    options={this.state.categoryOptions}
+                                                    getOptionValue={option => option.id}
+                                                    getOptionLabel={option => option.categoryNameViLevel3}
+                                                    value={this.state.categoriesFilter}
+                                                    onChange={e => {
+                                                        this.setState({
+                                                            categoriesFilter: e
+                                                        });
+                                                    }}
+                                                />
+                                                <span>
+                                                    {__(this.messages, "Thư mục")}
+                                                </span>
+                                            </Label>
+                                        </Colxx>
+                                    </Row>
+                                </CardBody>
+                                <CardFooter className="text-right">
+                                    <Button
+                                        onClick={this.searchProducts}
+                                    >
+                                        {__(this.messages, "Tìm kiếm")}
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        </Colxx>
+                    </Row>
+                    <Row>
+          <Colxx xxs="12">
+            <Card>
               
-            </Colxx>
-          </Row>
+            </Card>
+          </Colxx>
+        </Row>
 
-        </Fragment>
-      </div>
-    );
-  }
+                </Fragment>
+            </div>
+        );
+    }
 }
 
 export default injectIntl(Product);
